d1fe552f97fcb329ccc75a72776fb42a3bdb4425
kls6378 <kls6378@naver.com>
2020-06-05 12:44:27 +0900

    ah

--- himnaeseyo/main/models.py
+++ himnaeseyo/main/models.py
@@ -9,3 +9,9 @@ class Card(models.Model):
 
     def __str__(self):
         return self.contents +' | '+ self.templates
+
+class TEST(models.Model):
+    title = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.title
